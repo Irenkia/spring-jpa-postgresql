ca4707bfe1465f92b7818fe0a177401214e3ade9
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-10 16:54:39 +0200

    Build a CRUD App

---
+++ src/main/java/com/example/springjpapostgresql/repository/TutorialRepository.java
@@ -0,0 +1,18 @@
+package com.example.springjpapostgresql.repository;
+
+import com.example.springjpapostgresql.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TutorialRepository  extends JpaRepository<Tutorial, Long> {
+     List<Tutorial> findByTitleContaining(String title);
+
+//    @Query("SELECT t FROM Tutorial t WHERE t.title like %?1%")
+//    List<Tutorial> findByLike(String title);
+
+     List<Tutorial> findByPublished(boolean published);
+}
---
+++ src/main/java/com/example/springjpapostgresql/service/TutorialService.java
@@ -0,0 +1,37 @@
+package com.example.springjpapostgresql.service;
+
+import com.example.springjpapostgresql.model.Tutorial;
+import com.example.springjpapostgresql.repository.TutorialRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TutorialService {
+    private final TutorialRepository tutorialRepository;
+
+    public List<Tutorial> getAllTutorials() {
+        return tutorialRepository.findAll();
+    }
+
+    public Optional<Tutorial> getTutorialByIdr(long id) {
+        return tutorialRepository.findById(id);
+    }
+
+    public Tutorial createTutorial(Tutorial tutorial) {
+        return tutorialRepository.save(tutorial);
+    }
+
+
+    public void deleteTutorial(long id) {
+        tutorialRepository.deleteById(id);
+    }
+
+    public void deleteAllTutorials() {
+        tutorialRepository.deleteAll();
+    }
+}
